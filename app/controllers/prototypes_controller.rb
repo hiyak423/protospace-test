6090efe0a3fe381f9b5e8fcba97925c130dd3023
hiyak423 <hiyako0123@icloud.com>
2023-07-26 22:23:23 +0900

    テキスト・画像投稿機能実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,5 +3,24 @@ class PrototypesController < ApplicationController
   def index
     @prototypes = Prototype.all
   end
+  
+  def new
+    @prototype = Prototype.new
+  end
 
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      # 保存成功時の処理
+      redirect_to '/', notice: 'Prototypeが正常に作成されました。'
+    else
+      # 保存失敗時の処理
+      render :new
+    end
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :image, :concept, :catch_copy).merge(user: current_user.id)
+  end
 end
